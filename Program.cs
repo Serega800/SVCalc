548d828d509b0f4ba479ed9bf6dbfca1d07d0d1e
Serega800 <svs3107@gmail.com>
2021-09-14 20:45:10 +0500

    continue build parsers

--- Program.cs
+++ Program.cs
@@ -21,4 +21,71 @@ namespace SVCalc
             }
         }
     }
+    enum SyntaxKind
+    {
+        NumberToken,
+        WhiteSpaceToken
+    }
+    class SyntaxToken
+    {
+        public SyntaxToken(SyntaxKind kind, int position, string text, object value)
+        {
+            Kind = kind;
+            Position = position;
+            Text = text;
+        }
+        public SyntaxKind Kind { get; }
+        public int Position { get; }
+        public string Text{ get; }
+    }
+    class Lexer
+    {
+        private readonly string _text;
+        private int _position;
+        public Lexer(string text)
+        {
+            _text = text;
+        }
+        public char Current
+        {
+            get
+            {
+                if (_position >= _text.Length)
+                    return '\0';
+                return _text[_position];
+            }
+        }
+        private void Next()
+        {
+            _position++;
+        }
+        public SyntaxToken NextToken()
+        {
+            // <numbers>
+            // +-*/()
+            // <whitespace>
+            if (char.IsDigit(Current))
+            {
+                var start = _position;
+                while (char.IsDigit(Current))
+                    Next();
+
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                int.TryParse(text, out var value);
+                return new SyntaxToken(SyntaxKind.NumberToken, start, text, value);
+            }
+            if (char.IsWhiteSpace(Current))
+            {
+                var start = _position;
+                while (char.IsWhiteSpace(Current))
+                    Next();
+
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                int.TryParse(text, out var value);
+                return new SyntaxToken(SyntaxKind.WhiteSpaceToken, start, text, value);
+            }
+        }
+    }
 }
