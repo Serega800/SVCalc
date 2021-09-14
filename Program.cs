459d3d2903b473e4a055f589f47a2654a5daae63
Serega800 <svs3107@gmail.com>
2021-09-14 19:11:45 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SVCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
