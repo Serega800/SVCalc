c4f0eb378c1ee209b0b437b325dd6f82d001024a
Serega800 <svs3107@gmail.com>
2021-09-18 21:04:58 +0500

    classes administrated to smaller files. Wirk is finished for now.

---
+++ CodeAnalysis/ParenthesizedExpressionSyntax.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace SVCalc.CodeAnalysis
+{
+    sealed class ParenthesizedExpressionSyntax : ExpressionSyntax
+    {
+        public ParenthesizedExpressionSyntax(SyntaxToken openParanthesisToken, ExpressionSyntax expression, SyntaxToken closeParanthesisToken)
+        {
+            OpenParanthesisToken = openParanthesisToken;
+            Expression = expression;
+            CloseParanthesisToken = closeParanthesisToken;
+        }
+
+        public override SyntaxKind Kind => SyntaxKind.ParenthesizedExpression;
+        public SyntaxToken OpenParanthesisToken { get; }
+        public ExpressionSyntax Expression { get; }
+        public SyntaxToken CloseParanthesisToken { get; }
+
+
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return OpenParanthesisToken;
+            yield return Expression;
+            yield return CloseParanthesisToken;
+        }
+    }
+}
---
+++ CodeAnalysis/SyntaxTree.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SVCalc.CodeAnalysis
+{
+    sealed class SyntaxTree
+    {
+        public SyntaxTree(IEnumerable<string> diagnostics, ExpressionSyntax root, SyntaxToken endOfFileToken)
+        {
+            Diagnostics = diagnostics.ToArray();
+            Root = root;
+            EndOFileToken = endOfFileToken;
+        }
+
+        public IReadOnlyList<string> Diagnostics { get; }
+        public ExpressionSyntax Root { get; }
+        public SyntaxToken EndOFileToken { get; }
+        public static SyntaxTree Parse(string text)
+        {
+            var parser = new Parser(text);
+            return parser.Parse();
+        }
+    }
+}
